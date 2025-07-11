06828cf0da9471661ce0aaae73e787348d315299
Ryonis <143865189+Ryonis@users.noreply.github.com>
2025-07-11 20:44:56 +0330

    Add files via upload

--- app/src/main/java/com/tabadol/MainActivity.kt
+++ app/src/main/java/com/tabadol/MainActivity.kt
@@ -1,11 +1,3 @@
 package com.tabadol
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+class MainActivity {}
